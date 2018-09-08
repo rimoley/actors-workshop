493a46aa63eb68e65e6c4e613d31bda671fb0c8a
rubencg <ruben.carpinterogago@zooplus.com>
2018-09-08 23:53:35 +0200

    Add base elements for first hand off in scala

---
+++ src/main/scala/org/rutiger/theatre/App.scala
@@ -0,0 +1,14 @@
+package org.rutiger.theatre
+
+object App {
+
+  /*
+  1) Create actor system
+  2) create 2 actors form that contexte
+  3) send a couple of message
+  4) Run it
+   */
+  def main(args: Array[String]): Unit = {
+    println("Welcome")
+  }
+}
---
+++ src/main/scala/org/rutiger/theatre/actors/Legolas.scala
@@ -0,0 +1,22 @@
+package org.rutiger.theatre.actors
+
+import akka.actor.Actor
+
+/*
+This class represents a guy that can shots 5 arrows at once. Epicness level over 9k
+ */
+class Legolas extends Actor{
+
+  /*
+  1) Create couple of message that the actor will receive
+  2) Modify receive function to treat those messages and print some message
+  3) Create Gimli Actor, with a couple of messages
+  4) Legolas and Gimli should know each other? Select to use constructor or message block
+   */
+  override def receive: Receive = ???
+}
+
+object Legolas {
+
+  final case class RecountingEnemies()
+}
