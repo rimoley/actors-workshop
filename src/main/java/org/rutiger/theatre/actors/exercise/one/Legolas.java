04c8bee547fa69d805afdb31c648d823a78423cd
rubencg <ruben.carpinterogago@zooplus.com>
2018-09-29 18:16:57 +0200

    First commit for solution 3 java

--- src/main/java/org/rutiger/theatre/actors/exercise/one/Legolas.java
+++ src/main/java/org/rutiger/theatre/actors/exercise/one/Legolas.java
@@ -1,6 +1,7 @@
 package org.rutiger.theatre.actors.exercise.one;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
@@ -8,20 +9,60 @@ import org.rutiger.theatre.Messages;
 import org.rutiger.theatre.actors.exercise.Companion;
 
 /*
-This class represents a guy that can shots 5 arrows at once. Epicness level over 9k
+This class represents a guy that can shots 7 arrows at once. Epicness level over 9k
  */
 public class Legolas extends AbstractActor implements Companion {
+    private static Integer RESTING_TURNS = 3;
+    private static Integer MAX_ARROWS = 7;
     private LoggingAdapter log = Logging.getLogger(getContext().system(), this);
     private Integer counter = 0;
+    private ActorRef friend;
+
+    private Legolas(ActorRef friend) {
+        this.friend = friend;
+    }
 
     @Override
     public Receive createReceive() {
+        return onDuty().orElse(commonBehavior());
+    }
+
+    public Receive onDuty() {
         return receiveBuilder()
                 .match(Messages.Attack.class, msg -> {
-                    int arrowsShot = attackWith(5);
+                    int arrowsShot = attackWith(MAX_ARROWS);
                     log.info("shot {} arrows", arrowsShot);
                     getSender().tell(new Messages.Shot(arrowsShot), getSelf());
+                    if (arrowsShot > 5) {
+                        log.info("too tired after {} arrows", arrowsShot);
+                        getContext().become(tired(RESTING_TURNS).orElse(commonBehavior()));
+                    }
                 })
+                .build();
+    }
+
+    private Receive tired(Integer waiting) {
+        return receiveBuilder()
+                .match(Messages.Attack.class, attack -> {
+                    log.info("Cannot do anything... protect me friend");
+                    friend.forward(attack, getContext());
+                    if (waiting < 1) {
+                        getContext().become(onDuty().orElse(commonBehavior()));
+                        log.info("Feeling refreshed!!");
+                    }
+                    else {
+                        getContext().become(tired(waiting - 1).orElse(commonBehavior()));
+                    }
+                })
+                .match(Messages.EndBattle.class, any -> {
+                    log.info("Hey i killed {} orcs", counter);
+                    getContext().stop(getSelf());
+                })
+                .build();
+    }
+
+    private Receive commonBehavior() {
+        return receiveBuilder()
                 .match(Messages.Killed.class, killed -> {
                     counter += killed.orcs();
                 })
@@ -32,7 +73,7 @@ public class Legolas extends AbstractActor implements Companion {
                 .build();
     }
 
-    public static Props props() {
-        return Props.create(Legolas.class);
+    public static Props props(ActorRef friend) {
+        return Props.create(Legolas.class, friend);
     }
 }
--- src/main/java/org/rutiger/theatre/actors/exercise/third/BattleScribe.java
+++ src/main/java/org/rutiger/theatre/actors/exercise/third/BattleScribe.java
@@ -1,8 +1,11 @@
 package org.rutiger.theatre.actors.exercise.third;
 
 import akka.actor.AbstractActor;
+import akka.actor.DeadLetter;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.japi.pf.ReceiveBuilder;
+import org.rutiger.theatre.Messages;
 
 
 public class BattleScribe extends AbstractActor {
@@ -12,14 +15,22 @@ public class BattleScribe extends AbstractActor {
     //Receive a dead letter message and process it.
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match(DeadLetter.class, dlm -> {
+                    String message = String.format("Cannot deliver message '%s' from %s to %s",
+                            dlm.getClass().getName(), dlm.sender().path(), dlm.recipient().path());
+                    log.info("Battle LOG: {}", message);
+                    getContext().getSystem().eventStream().publish(message);
+                }).build();
     }
 
     @Override
     public void preStart() throws Exception {
+        getContext().getSystem().eventStream().subscribe(getSelf(), DeadLetter.class);
     }
 
     @Override
     public void postStop() throws Exception {
+        getContext().getSystem().eventStream().unsubscribe(getSelf());
     }
 }
